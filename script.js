1b339aad91dac5d90b278dd21497d19b4355edab
Rupesh Kumar <rkchourasia2001@gmail.com>
2023-03-25 02:06:51 +0530

    initial

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+// to pop up about us icon
+
+window.onload = function () {
+  setTimeout(appeardiv, 5000);
+}
+
+function appeardiv() {
+  document.getElementById('appear_div').style.display = "block";
+}
+
+function disapper(){
+  document.getElementById('appear_div').style.display = "none";
+}
+
+// -------slider navbar-----------
+
+
+
+
+
+// ----------------------for gallery slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
